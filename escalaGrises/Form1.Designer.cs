e48fe52b717ae9e62cc43313e0ef1a7d2232ff76
MarcoAntonioRodriguezHernandez <marco_1322134127@uptecamac.edu.mx>
2023-05-31 15:20:39 -0600

    Primer commit

---
+++ escalaGrises/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace escalaGrises
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pb_color = new System.Windows.Forms.PictureBox();
+            this.pb_blancoNegro = new System.Windows.Forms.PictureBox();
+            this.btn_cargar = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pb_bordes = new System.Windows.Forms.PictureBox();
+            this.pb_binaria = new System.Windows.Forms.PictureBox();
+            this.btn_grises = new System.Windows.Forms.Button();
+            this.btn_binaria = new System.Windows.Forms.Button();
+            this.btn_bordes = new System.Windows.Forms.Button();
+            this.btn_histograma = new System.Windows.Forms.Button();
+            this.btn_etiquetar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_color)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_blancoNegro)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_bordes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_binaria)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb_color
+            // 
+            this.pb_color.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pb_color.Location = new System.Drawing.Point(15, 16);
+            this.pb_color.Name = "pb_color";
+            this.pb_color.Size = new System.Drawing.Size(369, 265);
+            this.pb_color.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_color.TabIndex = 0;
+            this.pb_color.TabStop = false;
+            // 
+            // pb_blancoNegro
+            // 
+            this.pb_blancoNegro.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pb_blancoNegro.Location = new System.Drawing.Point(409, 16);
+            this.pb_blancoNegro.Name = "pb_blancoNegro";
+            this.pb_blancoNegro.Size = new System.Drawing.Size(369, 265);
+            this.pb_blancoNegro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_blancoNegro.TabIndex = 0;
+            this.pb_blancoNegro.TabStop = false;
+            // 
+            // btn_cargar
+            // 
+            this.btn_cargar.Location = new System.Drawing.Point(87, 287);
+            this.btn_cargar.Name = "btn_cargar";
+            this.btn_cargar.Size = new System.Drawing.Size(185, 61);
+            this.btn_cargar.TabIndex = 3;
+            this.btn_cargar.Text = "Imagen Original";
+            this.btn_cargar.UseVisualStyleBackColor = true;
+            this.btn_cargar.Click += new System.EventHandler(this.btn_cargar_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 359);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1542, 265);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // pb_bordes
+            // 
+            this.pb_bordes.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pb_bordes.Location = new System.Drawing.Point(1188, 16);
+            this.pb_bordes.Name = "pb_bordes";
+            this.pb_bordes.Size = new System.Drawing.Size(369, 265);
+            this.pb_bordes.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_bordes.TabIndex = 5;
+            this.pb_bordes.TabStop = false;
+            // 
+            // pb_binaria
+            // 
+            this.pb_binaria.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pb_binaria.Location = new System.Drawing.Point(800, 16);
+            this.pb_binaria.Name = "pb_binaria";
+            this.pb_binaria.Size = new System.Drawing.Size(369, 265);
+            this.pb_binaria.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_binaria.TabIndex = 6;
+            this.pb_binaria.TabStop = false;
+            // 
+            // btn_grises
+            // 
+            this.btn_grises.Location = new System.Drawing.Point(480, 287);
+            this.btn_grises.Name = "btn_grises";
+            this.btn_grises.Size = new System.Drawing.Size(209, 60);
+            this.btn_grises.TabIndex = 7;
+            this.btn_grises.Text = "Escala Grises";
+            this.btn_grises.UseVisualStyleBackColor = true;
+            this.btn_grises.Click += new System.EventHandler(this.btn_grises_Click);
+            // 
+            // btn_binaria
+            // 
+            this.btn_binaria.Location = new System.Drawing.Point(875, 287);
+            this.btn_binaria.Name = "btn_binaria";
+            this.btn_binaria.Size = new System.Drawing.Size(209, 60);
+            this.btn_binaria.TabIndex = 7;
+            this.btn_binaria.Text = "Binaria";
+            this.btn_binaria.UseVisualStyleBackColor = true;
+            this.btn_binaria.Click += new System.EventHandler(this.btn_binaria_Click);
+            // 
+            // btn_bordes
+            // 
+            this.btn_bordes.Location = new System.Drawing.Point(1270, 287);
+            this.btn_bordes.Name = "btn_bordes";
+            this.btn_bordes.Size = new System.Drawing.Size(209, 60);
+            this.btn_bordes.TabIndex = 7;
+            this.btn_bordes.Text = "Bordes";
+            this.btn_bordes.UseVisualStyleBackColor = true;
+            this.btn_bordes.Click += new System.EventHandler(this.btn_bordes_Click);
+            // 
+            // btn_histograma
+            // 
+            this.btn_histograma.Location = new System.Drawing.Point(112, 630);
+            this.btn_histograma.Name = "btn_histograma";
+            this.btn_histograma.Size = new System.Drawing.Size(160, 56);
+            this.btn_histograma.TabIndex = 8;
+            this.btn_histograma.Text = "Histograma";
+            this.btn_histograma.UseVisualStyleBackColor = true;
+            this.btn_histograma.Click += new System.EventHandler(this.btn_histograma_Click);
+            // 
+            // btn_etiquetar
+            // 
+            this.btn_etiquetar.Location = new System.Drawing.Point(387, 626);
+            this.btn_etiquetar.Name = "btn_etiquetar";
+            this.btn_etiquetar.Size = new System.Drawing.Size(209, 60);
+            this.btn_etiquetar.TabIndex = 9;
+            this.btn_etiquetar.Text = "Etiquetar";
+            this.btn_etiquetar.UseVisualStyleBackColor = true;
+            this.btn_etiquetar.Click += new System.EventHandler(this.btn_etiquetar_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1585, 741);
+            this.Controls.Add(this.btn_etiquetar);
+            this.Controls.Add(this.btn_histograma);
+            this.Controls.Add(this.btn_bordes);
+            this.Controls.Add(this.btn_binaria);
+            this.Controls.Add(this.btn_grises);
+            this.Controls.Add(this.pb_binaria);
+            this.Controls.Add(this.pb_bordes);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.btn_cargar);
+            this.Controls.Add(this.pb_blancoNegro);
+            this.Controls.Add(this.pb_color);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pb_color)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_blancoNegro)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_bordes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_binaria)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb_color;
+        private System.Windows.Forms.PictureBox pb_blancoNegro;
+        private System.Windows.Forms.Button btn_cargar;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.PictureBox pb_bordes;
+        private System.Windows.Forms.PictureBox pb_binaria;
+        private System.Windows.Forms.Button btn_grises;
+        private System.Windows.Forms.Button btn_binaria;
+        private System.Windows.Forms.Button btn_bordes;
+        private System.Windows.Forms.Button btn_histograma;
+        private System.Windows.Forms.Button btn_etiquetar;
+    }
+}
+
---
+++ escalaGrises/Form1.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace escalaGrises
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bitmap, final;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_cargar_Click(object sender, EventArgs e)
+        {
+            //Ventana para cargar archivos
+            OpenFileDialog o = new OpenFileDialog();
+            o.Filter = "JPG(*.jpg)|*.jpg|PNG (*.png)|*.png|Todos los archivos (*.*)|*.*";
+            //Archivo valido
+            if (o.ShowDialog() == DialogResult.OK)
+            {
+                //Cargar imagen
+                bitmap = new Bitmap(Image.FromFile(o.FileName));
+                //Mostrar imagen a color
+                pb_color.Image = bitmap;
+            }
+        }
+        private void btn_grises_Click(object sender, EventArgs e)
+        {
+            int w = bitmap.Width;
+            int h = bitmap.Height;
+            int pixelGris = 0;
+            Color actual, nuevoColor;
+            //Crear nuevo bitmap
+            final = new Bitmap(w, h);
+            //Recorrer pixel de imagen
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    actual = bitmap.GetPixel(i, j);
+                    //Color base para sacar gris (Rojo)
+                    //int pixelGris = 255 - (int)(0.3 * actual.R + 0.59 * actual.G + 0.11 * actual.B);
+                    pixelGris = (int)(0.3 * actual.R + 0.59 * actual.G + 0.11 * actual.B);
+
+                    nuevoColor = Color.FromArgb(pixelGris, pixelGris, pixelGris);
+                    //Guardar pixel con nuevo color
+                    final.SetPixel(i, j, nuevoColor);
+                }
+            }
+            pb_blancoNegro.Image = final;
+        }
+
+        private void btn_binaria_Click(object sender, EventArgs e)
+        {
+            int w = bitmap.Width;
+            int h = bitmap.Height;
+            int umbral = 128; // umbral de binarización
+            Color actual, nuevoColor;
+            //Crear nuevo bitmap
+            final = new Bitmap(w, h);
+            //Recorrer pixel de imagen
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    actual = bitmap.GetPixel(i, j);
+                    int pixelGris = (int)(0.3 * actual.R + 0.59 * actual.G + 0.11 * actual.B);
+                    // Convertir a blanco o negro dependiendo del umbral
+                    if (pixelGris >= umbral)
+                    {
+                        nuevoColor = Color.White;
+                    }
+                    else
+                    {
+                        nuevoColor = Color.Black;
+                    }
+                    //Guardar pixel con nuevo color
+                    final.SetPixel(i, j, nuevoColor);
+                }
+            }
+            pb_binaria.Image = final;
+        }
+        private void btn_bordes_Click(object sender, EventArgs e)
+        {
+            /*
+             CONCEPTOS UTILIZADOS:
+            El gradiente es una medida de la intensidad de cambio en una imagen. En el contexto de detección de bordes, el gradiente representa la magnitud de la diferencia de intensidad entre dos píxeles vecinos en la dirección perpendicular al borde.
+
+            El kernel que utilicé es un filtro de convolución que se utiliza para calcular el gradiente de la imagen. Específicamente, utilicé el kernel de Sobel, que consiste en dos matrices de 3x3 que se utilizan para calcular las derivadas parciales horizontales y verticales de la imagen:
+
+            Kernel horizontal:
+
+            |-1 0 1|
+            |-2 0 2|
+            |-1 0 1|
+
+            Kernel vertical:
+
+            |1 2 1|
+            |0 0 0|
+            |-1 -2 -1|
+
+            El kernel horizontal se utiliza para detectar bordes verticales en la imagen, mientras que el kernel vertical se utiliza para detectar bordes horizontales. Al combinar los resultados de ambos kernels, se puede obtener una imagen que destaque los bordes de la imagen original.             
+             */
+            int w = bitmap.Width;
+            int h = bitmap.Height;
+            int[,] kernelHorizontal = { { -1, 0, 1 }, { -2, 0, 2 }, { -1, 0, 1 } };
+            int[,] kernelVertical = { { 1, 2, 1 }, { 0, 0, 0 }, { -1, -2, -1 } };
+            Color actual, nuevoColor;
+            //Crear nuevo bitmap
+            final = new Bitmap(w, h);
+            //Recorrer pixel de imagen
+            for (int i = 1; i < w - 1; i++)
+            {
+                for (int j = 1; j < h - 1; j++)
+                {
+                    int pixelGrisHorizontal = 0;
+                    int pixelGrisVertical = 0;
+                    for (int ki = 0; ki < 3; ki++)
+                    {
+                        for (int kj = 0; kj < 3; kj++)
+                        {
+                            actual = bitmap.GetPixel(i + ki - 1, j + kj - 1);
+                            int pixelGris = (int)(0.3 * actual.R + 0.59 * actual.G + 0.11 * actual.B);
+                            pixelGrisHorizontal += pixelGris * kernelHorizontal[ki, kj];
+                            pixelGrisVertical += pixelGris * kernelVertical[ki, kj];
+                        }
+                    }
+                    // Calcular el gradiente
+                    int gradiente = (int)Math.Sqrt(pixelGrisHorizontal * pixelGrisHorizontal + pixelGrisVertical * pixelGrisVertical);
+                    gradiente = Math.Min(255, gradiente); // Limitar a 255
+                    nuevoColor = Color.FromArgb(gradiente, gradiente, gradiente);
+                    //Guardar pixel con nuevo color
+                    final.SetPixel(i, j, nuevoColor);
+                }
+            }
+            pb_bordes.Image = final;
+        }
+
+        private void btn_etiquetar_Click_1(object sender, EventArgs e) {
+
+            /*int w = bitmap.Width;
+            int h = bitmap.Height;
+            Color actual, nuevoColor;
+            int etiqueta = 1;
+            int[,] etiquetas = new int[w, h];
+
+            // Crear nuevo bitmap
+            final = new Bitmap(w, h);
+
+            // Recorrer píxeles de la imagen
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    actual = bitmap.GetPixel(i, j);
+
+                    // Verificar si el píxel es negro
+                    if (actual == Color.Black)
+                    {
+                        // Obtener las etiquetas de los píxeles vecinos
+                        List<int> etiquetasVecinos = new List<int>();
+
+                        // Verificar vecino superior
+                        if (j > 0)
+                        {
+                            etiquetasVecinos.Add(etiquetas[i, j - 1]);
+                        }
+
+                        // Verificar vecino izquierdo
+                        if (i > 0)
+                        {
+                            etiquetasVecinos.Add(etiquetas[i - 1, j]);
+                        }
+
+                        // Verificar vecino superior izquierdo
+                        if (i > 0 && j > 0)
+                        {
+                            etiquetasVecinos.Add(etiquetas[i - 1, j - 1]);
+                        }
+
+                        // Verificar vecino superior derecho
+                        if (i < w - 1 && j > 0)
+                        {
+                            etiquetasVecinos.Add(etiquetas[i + 1, j - 1]);
+                        }
+
+                        // Verificar etiquetas de vecinos
+                        if (etiquetasVecinos.Count == 0)
+                        {
+                            // No hay vecinos con etiqueta
+                            etiquetas[i, j] = etiqueta;
+                            etiqueta++;
+                        }
+                        else
+                        {
+                            // Asignar etiqueta mínima a los vecinos
+                            etiquetas[i, j] = etiquetasVecinos.Min();
+
+                            // Actualizar etiquetas de vecinos
+                            foreach (int etiquetaVecino in etiquetasVecinos)
+                            {
+                                if (etiquetaVecino != etiquetas[i, j])
+                                {
+                                    // Actualizar etiqueta
+                                    for (int x = 0; x < w; x++)
+                                    {
+                                        for (int y = 0; y < h; y++)
+                                        {
+                                            if (etiquetas[x, y] == etiquetaVecino)
+                                            {
+                                                etiquetas[x, y] = etiquetas[i, j];
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+            }/*
+            // Generar colores aleatorios para las etiquetas
+            Random rand = new Random();
+            Dictionary<int, Color> coloresEtiquetas = new Dictionary<int, Color>();
+
+            for (int x = 1; x < etiqueta; x++)
+            {
+                // Generar color aleatorio
+                Color colorEtiqueta = Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256));
+
+                // Asignar color a la etiqueta
+                coloresEtiquetas[x] = colorEtiqueta;
+            }
+
+            // Asignar colores a los píxeles etiquetados
+            for (int p = 0; p < w; p++)
+            {
+                for (int o = 0; o < h; o++)
+                {
+                    if (etiquetas[p, o] != 0)
+                    {
+                        nuevoColor = coloresEtiquetas[etiquetas[p, o]];
+                    }
+                    else
+                    {
+                        nuevoColor = Color.White;
+                    }
+
+                    // Guardar pixel con nuevo color
+                    final.SetPixel(p, o, nuevoColor);
+                }
+            }
+        }
+        pb_etiquetas.Image = final;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < w; i++)
+        {
+            for (int j = 0; j < h; j++)
+            {
+                sb.Append(etiquetas[i, j]);
+                sb.Append("\t");
+            }
+            sb.AppendLine();
+        }
+
+        // Guardar archivo de texto
+        SaveFileDialog s = new SaveFileDialog();
+        s.Filter = "Archivo de texto (*.txt)|*.txt";
+        if (s.ShowDialog() == DialogResult.OK)
+        {
+            string filePath = s.FileName;
+            System.IO.File.WriteAllText(filePath, sb.ToString());
+        }
+    }*/
+
+            int w = bitmap.Width;
+            int h = bitmap.Height;
+            int etiqueta = 1;
+            int[,] etiquetas = new int[w, h];
+
+            // Etiquetado de regiones utilizando el algoritmo iterativo
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    if (final.GetPixel(i, j).ToArgb() == Color.White.ToArgb())
+                    {
+                        if (i > 0 && j > 0 && etiquetas[i - 1, j] != 0)
+                        {
+                            etiquetas[j, i] = etiquetas[i - 1, j];
+                        }
+                        else if (i > 0 && j > 0 && etiquetas[i, j - 1] != 0)
+                        {
+                            etiquetas[j, i] = etiquetas[i, j - 1];
+                        }
+                        else
+                        {
+                            etiquetas[j, i] = etiqueta;
+                            etiqueta++;
+                        }
+                    }
+                }
+            }
+
+            // Exportar etiquetas a un archivo de texto
+            SaveFileDialog s = new SaveFileDialog();
+            s.Filter = "Archivo de texto (*.txt)|*.txt";
+
+            if (s.ShowDialog() == DialogResult.OK)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i < h; i++)
+                {
+                    for (int j = 0; j < w; j++)
+                    {
+                        sb.Append(etiquetas[i, j] + " ");
+                    }
+                    sb.AppendLine();
+                }
+
+                File.WriteAllText(s.FileName, sb.ToString());
+            }
+        
+    }
+ 
+
+        private void btn_histograma_Click(object sender, EventArgs e)
+        {
+            int w = bitmap.Width;
+            int h = bitmap.Height;
+            int[,] kernelHorizontal = { { -1, 0, 1 }, { -2, 0, 2 }, { -1, 0, 1 } };
+            int[,] kernelVertical = { { 1, 2, 1 }, { 0, 0, 0 }, { -1, -2, -1 } };
+            Color actual, nuevoColor;
+            //Crear nuevo bitmap
+            final = new Bitmap(w, h);
+            //Recorrer pixel de imagen y calcular el histograma
+            int[] histogramaRojo = new int[256];
+            int[] histogramaVerde = new int[256];
+            int[] histogramaAzul = new int[256];
+            for (int i = 1; i < w - 1; i++)
+            {
+                for (int j = 1; j < h - 1; j++)
+                {
+                    actual = bitmap.GetPixel(i, j);
+                    int pixelGris = (int)(0.3 * actual.R + 0.59 * actual.G + 0.11 * actual.B);
+                    histogramaRojo[actual.R]++;
+                    histogramaVerde[actual.G]++;
+                    histogramaAzul[actual.B]++;
+                    //...
+                }
+            }
+            //Crear el chart y agregar los datos del histograma
+            chart1.Series.Add("Rojo");
+            chart1.Series.Add("Verde");
+            chart1.Series.Add("Azul");
+            for (int i = 0; i < 256; i++)
+            {
+                chart1.Series["Rojo"].Points.AddXY(i, histogramaRojo[i]);
+                chart1.Series["Verde"].Points.AddXY(i, histogramaVerde[i]);
+                chart1.Series["Azul"].Points.AddXY(i, histogramaAzul[i]);
+            }
+            chart1.ChartAreas.Add("Histograma");
+            chart1.ChartAreas["Histograma"].AxisX.Title = "Intensidad";
+            chart1.ChartAreas["Histograma"].AxisY.Title = "Cantidad de píxeles";
+        }
+    }
+}
